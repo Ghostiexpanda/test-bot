edaf1a202f6e4caedbb1e489e928fb9f6f308c3d
Ghostiexpanda <trexmex3223@gmail.com>
2023-03-05 00:16:13 -0500

    first commit

--- commands/restart.js
+++ commands/restart.js
@@ -1,10 +1,32 @@
-const Discord = require('discord.js');
-const { spawn, exec } = require('child_process');
+const { spawn } = require('child_process');
 module.exports = {
-    name: "restart",
-    description: "restart bot",
+  name: "restart",
+  description: "restart the bot",
 
-    async exec(interaction) {
-        const embed = new MessageEmbed()
+  async execute(interaction) {
+
+    const ownerID = '893038810902958130'
+    if(interaction.user.id !== ownerID) {
+        interaction.reply({content: `You don't have the permission to use this command.`, ephemeral: true})
+
+    } else {
+
+        try {
+
+            interaction.reply({ content: `Restarting the bot ...`, ephemeral: true });
+            const subprocess = spawn('start', ['cmd.exe', '/c', 'node main.js'], {
+                shell: true
+            });
+
+            setTimeout(() => {
+                process.exit();
+            }, 5000);
+
+        } catch (e) {
+            console.log(e.message)
+        }
+        
     }
-}
+    
+  }
+};
